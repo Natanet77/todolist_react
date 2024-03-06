dfcfa6d8255a1c3e2ff9e521845f253267e87a7f
Natanet77 <natanet77@ukr.net>
2024-03-06 17:05:16 +0200

    end task for React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    setTodoList([...todoList, { text: newTodo, completed: false }]);
+    setNewTodo("");
+  };
+
+  const removeTodo = (index) => {
+    const updatedList = [...todoList];
+    updatedList.splice(index, 1);
+    return setTodoList(updatedList);
+  };
+
+  const toggleCompletion = (index) => {
+    const updatedList = [...todoList];
+    if ((updatedList[index].completed = !updatedList[index].completed)) {
+      return setTodoList(updatedList);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">TODO List</h1>
+      <div className="field">
+        <input
+          className="field__add"
+          type="text"
+          placeholder="Type new item..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="add" onClick={addTodo}>
+          Add
+        </button>
+      </div>
+      <button className="completed">Show/hide completed</button>
+      <ul className="items">
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleCompletion(index)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => removeTodo(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
